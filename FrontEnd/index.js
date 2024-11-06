def4e85862d5336314f331cd3288a0925a950308
EliseLHop <elise.lhopitault@gmail.com>
2024-11-06 09:32:33 +0100

    first commit

---
+++ FrontEnd/index.js
@@ -0,0 +1,7 @@
+console.log ("index.js");
+
+// let introduction=document.getElementById("introduction");
+// introduction.textContent="Bonjour";
+
+// let img=document.getElementById("image2");
+// img.src="assets/images/abajour-tahina.png";
